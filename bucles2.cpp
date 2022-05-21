e8d62d90c4159bfe1065473c02eed20391d4c0ab
CleverCondoriB <105955401+kleber88@users.noreply.github.com>
2022-05-20 22:25:48 -0500

    Create bucles2.cpp

---
+++ bucles2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+cout<<"Los numeros multiplos del 5 al 100 son :"<<endl;
+
+for(int i;i<=100;i++){
+if(5%i==0)
+cout<<"i"<<endl;
+}
+
+return 0;
+}
