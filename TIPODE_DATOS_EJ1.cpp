cb6d9a579a2c0c7ffdc75c89cdda012d9567ac9e
CleverCondoriB <105955401+kleber88@users.noreply.github.com>
2022-05-20 21:58:10 -0500

    Create TIPODE_DATOS_EJ1.cpp

---
+++ TIPODE_DATOS_EJ1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main(void){
+
+char nombre, ApellidoP, ApellidoM, primeraLetradelNombre, pLaM//EJERCICIO 1
+
+cout<<"Ingrese su nombre :"<<endl;
+cin>> nombre;
+
+cout<<"Ingrese la primera letra del nombre :"<<endl;
+cin>> primeraLetradelNombre;
+
+cout<<"Ingrese su apellido paterno :"<<endl;
+cin>> ApellidoP;
+
+cout<<"Ingrese su apellido materno :"<<endl;
+cin>> ApellidoM;
+
+cout<<"Ingrese la primera letra de sua apellido materno :"<<endl;
+cin>> pLaM;
+
+cout<<"Su correo unsa generado es :"<<endl;
+
+system("pause");
+
+return 0;
+
+
+}
+i
