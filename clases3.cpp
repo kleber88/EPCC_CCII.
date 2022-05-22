3fb8319b05fa99e4719115054a52de208c11067f
CleverCondoriB <105955401+kleber88@users.noreply.github.com>
2022-05-22 13:04:16 -0500

    Create clases3.cpp

---
+++ clases3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class Persona{
+	
+	private:
+		string nombre;
+		int edad;
+		int nacimiento;
+	
+	public:
+		Persona(string _nombre,int _edad,int _nacimeinto);
+		void nombre();
+		void edad();
+		void nacimiento();
+};
+
+
+
+int main(){
+	
+}
