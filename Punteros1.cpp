54d7fcdfb77513505500c08ef9c4d2d81d643e3a
CleverCondoriB <105955401+kleber88@users.noreply.github.com>
2022-05-22 12:17:10 -0500

    Create Punteros1.cpp

---
+++ Punteros1.cpp
@@ -0,0 +1,27 @@
+#include <conio.h>
+#include <stdio.h>
+
+int main()
+{
+    int aux, a, b;
+    
+    a= 2;
+    b= 3;
+    
+
+    cout << " El numero 1 vale: " << a << end1;
+    cout << " el numero 2 vale: " << b << end1;
+
+    cout << " Intercambiando los valores..." << end 1;
+
+    // Para hacer el intercambio utilizamos una variable auxiliar
+
+    auxiliar = un;
+    a = b;
+    b = auxiliar;
+
+    cout << "Ahora, el valor de v1 es: " << a << end1;
+    cout << "Ahora, el valor de v2 es: " << b << end1;
+
+    return 0;
+}
